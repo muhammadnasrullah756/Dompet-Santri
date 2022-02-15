c6fd26f2edf7da73d5479e6fba926c2c02cbcf51
Mikael025 <kimpoliyok@gmail.com>
2022-02-16 03:59:08 +0700

    CRUD katalog

--- app/Http/Controllers/KatalogController.php
+++ app/Http/Controllers/KatalogController.php
@@ -47,11 +47,11 @@ class KatalogController extends Controller
     {
         $barang = katalog::find($id);
 
-        $request->validate([
-            'gambar_barang' => 'required|mimes:png,jpg,jpeg|max:2048',
-            'nama_barang' => 'required|string',
-            'harga_barang' => 'required|string'
-        ]);
+        // $request->validate([
+        //     'gambar_barang' => 'required|mimes:png,jpg,jpeg|max:2048',
+        //     'nama_barang' => 'required|string',
+        //     'harga_barang' => 'required|string'
+        // ]);
 
         $image  = $request->file('gambar_barang');
         $result = CloudinaryStorage::replace($barang->gambar_barang, $image->getRealPath(), $image->getClientOriginalName());
