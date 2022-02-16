14cdf02d0408c275a7a98f9de925f3a846dc4008
muhammadnasrullah756 <muhammadnasrullah756@gmail.com>
2022-02-16 13:44:22 +0700

    get user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,6 +20,7 @@ class CreateUsersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->enum('roles',['merchant','customer','finance']);
+            $table->integer('balance')->default(0);
             $table->rememberToken();
             $table->timestamps();
         });
